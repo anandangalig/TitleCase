9b22bb3fde9b2ad67e64440d7d083bbe6c52c9d3
anand angalig, alexandre leibler and kyle fisher <alexandre@>
2016-09-12 15:36:16 -0400

    create additional test to cover strings with mixed capitalizations

--- src/TitleCaseGenerator.php
+++ src/TitleCaseGenerator.php
@@ -3,15 +3,11 @@
     class TitleCaseGenerator
     {
         private $title;
+        private $special_words = ["a", "an", "the", "at", "by", "for", "in", "of", "on", "to", "up", "and", "as", "but", "or", "nor"];
 
         function __construct($title="")
         {
             $this->title = $title;
-        }
-
-        function setTitle($new_title)
-        {
-            $this->title = (string) $new_name;
         }
 
         function getTitle()
@@ -19,17 +15,61 @@
             return $this->title;
         }
 
+        function setTitle($new_title)
+        {
+            $this->title = (string) $new_name;
+        }
+
+
+        function getSpecialWords() {
+            return $this->special_words;
+        }
+
+        function setSpecialWords($special_list) {
+            $this->special_words = (string) $special_list;
+        }
+
+
+        //NOTE: Sees if input word is one of the elements of $special_words array
+        function checkSpecialWords($word) {
+
+            $special_list = $this->getSpecialWords();
+
+            for ($special_index = 0; $special_index < count($special_list); $special_index++) {
+                if ($word === $special_list[$special_index]) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+
+
         //TODO: Work on this as we test one spec at a time
         function formatTitleCase($input_title)
         {
             $output_title = $input_title;
+            $output_title = strtolower($output_title);
             $output_title_words = explode(" ", $output_title);
+            
 
 
             /*NOTE: go through all words in input string and make first letter of each word upper case. The for-loop helps avoid single word tests failing*/
 
             for ($word_index = 0; $word_index < count($output_title_words); $word_index++) {
-                $output_title_words[$word_index][0] = strtoupper($output_title_words[$word_index][0]);
+
+                //NOTE: if current word is a special word
+                if ($this->checkSpecialWords($output_title_words[$word_index])) {
+
+                    //NOTE: if we are looking at either the first or the last word
+                    if ($word_index == 0 || $word_index == count($output_title_words) - 1) {
+                        $output_title_words[$word_index][0] = strtoupper($output_title_words[$word_index][0]);
+                    }
+                }
+                //NOTE: current word is not a special word
+                else {
+                    $output_title_words[$word_index][0] = strtoupper($output_title_words[$word_index][0]);
+                }
             }
 
             $output_title = implode(" ", $output_title_words);
--- tests/TitleCaseGeneratorTest.php
+++ tests/TitleCaseGeneratorTest.php
@@ -8,79 +8,104 @@
         function testSingleCharacter() {
             //ARRANGE
             $input = "t";
+            $expected_output = "T";
+            //NOTE: We need an instance in order to test the method we are interested in.
+            // Methods only exist on specific instances of objects
             $test_TitleCaseGenerator = new TitleCaseGenerator($input);
 
             //ACT
+            //NOTE: $test_result is what comes out of the black box (formatTitleCase method)
             $test_result = $test_TitleCaseGenerator->formatTitleCase($input);
 
             //ASSERT
             //NOTE: assertion checks: is $test_result === $our_desired_output
-            $this->assertEquals("T", $test_result);
+            $this->assertEquals($expected_output, $test_result);
         }
 
         // TEST SINGLE LOWER CASE WORD
         function testSingleWord() {
             //ARRANGE
             $input = "friendship";
+            $expected_output = "Friendship";
             $test_TitleCaseGenerator = new TitleCaseGenerator($input);
 
             //ACT
             $test_result = $test_TitleCaseGenerator->formatTitleCase($input);
 
             //ASSERT
-            $this->assertEquals("Friendship", $test_result);
+            $this->assertEquals($expected_output, $test_result);
         }
 
         // TEST MULTIPLE WORDS LOWER CASE WORDS WITH NO SPECIAL WORDS
         function testMultipleNoSpecial() {
             //ARRANGE
             $input = "american beauty";
+            $expected_output = "American Beauty";
             $test_TitleCaseGenerator = new TitleCaseGenerator($input);
 
             //ACT
             $test_result = $test_TitleCaseGenerator->formatTitleCase($input);
 
             //ASSERT
-            $this->assertEquals("American Beauty", $test_result);
+            $this->assertEquals($expected_output, $test_result);
         }
 
         // TEST MULTIPLE WORDS LOWER CASE WITH A SINGLE SPECIAL WORD AT THE BEGINNING OF THE TITLE
         function testMultipleSpecialAtStart() {
             //ARRANGE
             $input = "in bruges";
+            $expected_output = "In Bruges";
             $test_TitleCaseGenerator = new TitleCaseGenerator($input);
 
             //ACT
             $test_result = $test_TitleCaseGenerator->formatTitleCase($input);
 
             //ASSERT
-            $this->assertEquals("In Bruges", $test_result);
+            $this->assertEquals($expected_output, $test_result);
         }
 
         // TEST MULTIPLE WORDS LOWER CASE WITH SPECIAL WORD AT END OF TITLE
         function testMultipleSpecialAtEnd() {
             //ARRANGE
             $input = "the jig is up";
+            $expected_output = "The Jig Is Up";
             $test_TitleCaseGenerator = new TitleCaseGenerator($input);
 
             //ACT
             $test_result = $test_TitleCaseGenerator->formatTitleCase($input);
 
             //ASSERT
-            $this->assertEquals("The Jig Is Up", $test_result);
+            $this->assertEquals($expected_output, $test_result);
         }
 
         // TEST MULTIPLE WORDS LOWER CASE WITH SPECIAL SOMEWHERE IN THE MIDDLE
         function testMultipleSpecialInMiddle() {
             //ARRANGE
             $input = "the thing living in my closet but";
+            $expected_output = "The Thing Living in My Closet But";
+
             $test_TitleCaseGenerator = new TitleCaseGenerator($input);
 
             //ACT
             $test_result = $test_TitleCaseGenerator->formatTitleCase($input);
 
             //ASSERT
-            $this->assertEquals("The Thing Living in My Closet But", $test_result);
+            $this->assertEquals($expected_output, $test_result);
+        }
+
+        // TEST FOR MIXED CAPITALIZATION AMONG MULTIPLE WORDS INCLUDING SPECIAL WORDS
+        function testMixedCapitalsIncludeSpecial() {
+            //ARRANGE
+            $input = "tHe Thing LIVING iN MY CloSet BUt";
+            $expected_output = "The Thing Living in My Closet But";
+
+            $test_TitleCaseGenerator = new TitleCaseGenerator($input);
+
+            //ACT
+            $test_result = $test_TitleCaseGenerator->formatTitleCase($input);
+
+            //ASSERT
+            $this->assertEquals($expected_output, $test_result);
         }
     }
  ?>
