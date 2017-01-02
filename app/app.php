ad35b8f5916b54ef9d00fe850f9bc929fae97bcf
Anand Angalig <anandangalig@gmail.com>
2017-01-02 15:13:00 -0500

    add Start Over button and Bootstrap

--- app/app.php
+++ app/app.php
@@ -1,5 +1,4 @@
 <?php
-    /*NOTE: "php-tilda" __DIR__ refers to the root directory aka where you start your server aka /Users/Guest/Desktop/titleCase/web/Users/Guest/Desktop/titleCase/web*/
     require_once(__DIR__ . "/../vendor/autoload.php");
     require_once(__DIR__ . "/../src/TitleCaseGenerator.php");
 
@@ -7,7 +6,6 @@
     if (empty($_SESSION["titles"])) {
         $_SESSION["titles"] = array();
     }
-
 
     $app = new Silex\Application();
 
--- src/TitleCaseGenerator.php
+++ src/TitleCaseGenerator.php
@@ -30,7 +30,6 @@
         }
 
 
-        //NOTE: Sees if input word is one of the elements of $special_words array
         function checkSpecialWords($word) {
 
             $special_list = $this->getSpecialWords();
@@ -45,29 +44,18 @@
 
 
 
-        //TODO: Work on this as we test one spec at a time
         function formatTitleCase($input_title)
         {
             $output_title = $input_title;
             $output_title = strtolower($output_title);
             $output_title_words = explode(" ", $output_title);
 
-
-
-            /*NOTE: go through all words in input string and make first letter of each word upper case. The for-loop helps avoid single word tests failing*/
-
             for ($word_index = 0; $word_index < count($output_title_words); $word_index++) {
-
-                //NOTE: if current word is a special word
                 if ($this->checkSpecialWords($output_title_words[$word_index])) {
-
-                    //NOTE: if we are looking at either the first or the last word
                     if ($word_index == 0 || $word_index == count($output_title_words) - 1) {
                         $output_title_words[$word_index][0] = strtoupper($output_title_words[$word_index][0]);
                     }
                 }
-
-                //NOTE: current word is not a special word
                 else {
                     $output_title_words[$word_index][0] = strtoupper($output_title_words[$word_index][0]);
                 }
@@ -78,6 +66,4 @@
             return $output_title;
         }
     }
-
-
  ?>
